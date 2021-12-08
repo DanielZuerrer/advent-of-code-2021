a0d99af53451ddc41e46a4654ca0f735610566f9
Daniel Zürrer <daniel.zuerrer@digitecgalaxus.ch>
2021-12-08 13:10:11 +0100

    real day 3 b solution

--- day-3/b.py
+++ day-3/b.py
@@ -1,20 +1,32 @@
 with open('./input.txt', 'r', encoding='utf-8') as puzzle_input:
     lines = [line.strip() for line in puzzle_input.readlines()]
 
-number_count = len(lines)
 number_lengths = len(lines[0])
 
-def get_bit_sum(bit, numbers):
+def get_oxygen_generator_rating(bit, numbers):
+    if len(numbers) == 1:
+        return numbers[0]
+
     bits = [int(number[bit]) for number in numbers]
-    return sum(bits)
+    most_common = '1' if sum(bits) >= len(numbers) / 2 else '0'
+    numbers = list(filter(lambda n: n[bit] == most_common, numbers))
 
-gamma_rate = ''
-for bit in range(number_lengths):
-    bit_sum = get_bit_sum(bit, lines)
-    gamma_rate += ('1' if bit_sum > number_count / 2 else '0')
+    return get_oxygen_generator_rating(bit + 1, numbers)
 
-epsilon_rate = ''.join(['1' if b == '0' else '0' for b in gamma_rate])
-gamma_rate = int(gamma_rate, base=2)
-epsilon_rate = int(epsilon_rate, base=2)
+def get_CO2_scrubber_rating(bit, numbers):
+    if len(numbers) == 1:
+        return numbers[0]
 
-print('Solution:', gamma_rate * epsilon_rate)
+    bits = [int(number[bit]) for number in numbers]
+    least_common = '1' if sum(bits) < len(numbers) / 2 else '0'
+    numbers = list(filter(lambda n: n[bit] == least_common, numbers))
+
+    return get_CO2_scrubber_rating(bit + 1, numbers)
+
+oxygen_generator_rating = get_oxygen_generator_rating(0, lines)
+CO2_scrubber_rating = get_CO2_scrubber_rating(0, lines)
+
+oxygen_generator_rating = int(oxygen_generator_rating, base=2)
+CO2_scrubber_rating = int(CO2_scrubber_rating, base=2)
+
+print('Solution:', oxygen_generator_rating * CO2_scrubber_rating)
